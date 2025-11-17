94c7e6a9966d7cc9ac5a6090b563dd36e5af0077
Vikesh Rooplall <rooplallv50@gmail.com>
2025-11-17 09:27:45 +0400

    facilities controller completed

--- config/routes.rb
+++ config/routes.rb
@@ -27,4 +27,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   resources :donations, only: [:index, :show]
+  resources :facilities, only: [:index]
 end
