d7b7ba0b3f6eecd59216d43e20ddec07b42108c4
Vikesh Rooplall <rooplallv50@gmail.com>
2025-11-23 20:36:14 +0400

    Donor home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,15 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
+    @facilities = Facility.all
+
+    @markers = @facilities.geocoded.map do |facility|
+      {
+        lat: facility.latitude,
+        lng: facility.longitude,
+        info_window_html: render_to_string(partial: "facilities/info_window", locals: { facility: facility }),
+        marker_html: render_to_string(partial: "facilities/marker", locals: { facility: facility })
+      }
+    end
   end
 end
--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,7 +1,7 @@
 class Facility < ApplicationRecord
   has_many :blood_requests, dependent: :destroy
   has_many :donations, dependent: :destroy
-  has_many :inventories, dependent: :destroy
+  # has_many :inventories, dependent: :destroy
 
   validates :name, :address, :phone_number, presence: true
   enum :facility_type, { hospital: 0, clinic: 1, mobile_team: 2 }
