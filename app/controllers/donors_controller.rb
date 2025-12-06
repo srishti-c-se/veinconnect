1a0045795ff21e301d977637a3d5f82afd68818c
Tania Kwok <tania@pinkmojito.net>
2025-12-06 18:01:52 +0400

    TK_donor_index_6122025

--- app/controllers/donors_controller.rb
+++ app/controllers/donors_controller.rb
@@ -3,5 +3,14 @@ class DonorsController < ApplicationController
   def index
     # @donors = Donor.all # modify
     @donors = Donor.active.all
+
+    if params[:blood_type].present?
+      @donors = @donors.where(blood_type: params[:blood_type])
+    end
+
+    # Filter by location if any selected
+    if params[:location].present?
+      @donors = @donors.where(address: params[:location])
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'open-uri'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
 puts 'Cleaning database...'
 # Destroy all previous records
 Facility.destroy_all
@@ -71,34 +74,36 @@ facility54 = Facility.create(name: "University of Mauritius Blood Donation Centr
 facility55 = Facility.create(name: "Red Cross Society of Mauritius - Blood Donation Centre", address: "Ste Thérèse Street, Curepipe, Mauritius", phone_number: "6751975", facility_type: "blood_donation_center")
 
 puts '🌱 Seeding users...'
+puts '🌱 Seeding users...'
 
-user1 = User.create(first_name: "Aisha", last_name: "Ramdin", date_of_birth: "1993-04-12", email: "aisha.ramdin@example.com", password: "password123", phone: "57981234", address: "Quatre Bornes, Mauritius")
-user2 = User.create(first_name: "Kevin", last_name: "Appadoo", date_of_birth: "1988-11-22", email: "kevin.appadoo@example.com", password: "password123", phone: "58564321", address: "Port Louis, Mauritius")
-user3 = User.create(first_name: "Marie", last_name: "Duval", date_of_birth: "1990-02-03", email: "marie.duval@example.com", password: "password123", phone: "59210234", address: "Curepipe, Mauritius")
-user4 = User.create(first_name: "Vikash", last_name: "Rughoobur", date_of_birth: "1995-07-14", email: "vikash.rughoobur@example.com", password: "password123", phone: "57651209", address: "Vacoas, Mauritius")
-user5 = User.create(first_name: "Sarah", last_name: "Latour", date_of_birth: "1987-09-25", email: "sarah.latour@example.com", password: "password123", phone: "59482211", address: "Rose Hill, Mauritius")
-user6 = User.create(first_name: "Jason", last_name: "Ng", date_of_birth: "1998-01-19", email: "jason.ng@example.com", password: "password123", phone: "58091344", address: "Beau Bassin, Mauritius")
-user7 = User.create(first_name: "Priya", last_name: "Seetohul", date_of_birth: "1992-06-11", email: "priya.seetohul@example.com", password: "password123", phone: "57221198", address: "Flacq, Mauritius")
-user8 = User.create(first_name: "Miguel", last_name: "Da Silva", date_of_birth: "1985-12-02", email: "miguel.dasilva@example.com", password: "password123", phone: "59734122", address: "Moka, Mauritius")
-user9 = User.create(first_name: "Anjali", last_name: "Bhugaloo", date_of_birth: "1994-08-17", email: "anjali.bhugaloo@example.com", password: "password123", phone: "59120017", address: "Goodlands, Mauritius")
-user10 = User.create(first_name: "Dylan", last_name: "Clement", date_of_birth: "1999-03-28", email: "dylan.clement@example.com", password: "password123", phone: "57399821", address: "Mahebourg, Mauritius")
-user11 = User.create(first_name: "Stephanie", last_name: "Wong", date_of_birth: "1986-10-10", email: "stephanie.wong@example.com", password: "password123", phone: "59672210", address: "Port Louis, Mauritius")
-user12 = User.create(first_name: "Kunal", last_name: "Gopal", date_of_birth: "1991-05-05", email: "kunal.gopal@example.com", password: "password123", phone: "58412390", address: "Triolet, Mauritius")
-user13 = User.create(first_name: "Emma", last_name: "Ramasamy", date_of_birth: "2000-02-14", email: "emma.ramasamy@example.com", password: "password123", phone: "57982011", address: "Quatre Bornes, Mauritius")
-user14 = User.create(first_name: "Yannick", last_name: "Legrand", date_of_birth: "1984-04-30", email: "yannick.legrand@example.com", password: "password123", phone: "59564401", address: "Curepipe, Mauritius")
-user15 = User.create(first_name: "Lina", last_name: "Peerbux", date_of_birth: "1997-06-08", email: "lina.peerbux@example.com", password: "password123", phone: "57812209", address: "Vacoas, Mauritius")
-user16 = User.create(first_name: "Thierry", last_name: "Labonte", date_of_birth: "1989-01-04", email: "thierry.labonte@example.com", password: "password123", phone: "59382177", address: "Grand Baie, Mauritius")
-user17 = User.create(first_name: "Sanjana", last_name: "Mohit", date_of_birth: "1993-09-20", email: "sanjana.mohit@example.com", password: "password123", phone: "57720016", address: "Terre Rouge, Mauritius")
-user18 = User.create(first_name: "Lucas", last_name: "Robert", date_of_birth: "1990-03-10", email: "lucas.robert@example.com", password: "password123", phone: "59831190", address: "Port Louis, Mauritius")
-user19 = User.create(first_name: "Melissa", last_name: "d’Hotman", date_of_birth: "1996-07-01", email: "melissa.dhotman@example.com", password: "password123", phone: "57123098", address: "Floréal, Mauritius")
-user20 = User.create(first_name: "Harish", last_name: "Bheekhun", date_of_birth: "1988-08-09", email: "harish.bheekhun@example.com", password: "password123", phone: "57689012", address: "Phoenix, Mauritius")
-user21 = User.create(first_name: "Amira", last_name: "Rahim", date_of_birth: "1995-11-12", email: "amira.rahim@example.com", password: "password123", phone: "59071822", address: "Quatre Bornes, Mauritius")
-user22 = User.create(first_name: "Jonathan", last_name: "Perrine", date_of_birth: "1987-02-23", email: "jonathan.perrine@example.com", password: "password123", phone: "58280117", address: "Curepipe, Mauritius")
-user23 = User.create(first_name: "Shreya", last_name: "Oodit", date_of_birth: "1998-05-09", email: "shreya.oodit@example.com", password: "password123", phone: "59320178", address: "Moka, Mauritius")
-user24 = User.create(first_name: "Andrew", last_name: "André", date_of_birth: "1985-10-16", email: "andrew.andre@example.com", password: "password123", phone: "59911032", address: "Rose Hill, Mauritius")
-user25 = User.create(first_name: "Farah", last_name: "Abdool", date_of_birth: "1992-12-25", email: "farah.abdool@example.com", password: "password123", phone: "57192304", address: "Port Louis, Mauritius")
-user26 = User.create(first_name: "Ryan", last_name: "Cheung", date_of_birth: "1991-03-29", email: "ryan.cheung@example.com", password: "password123", phone: "58870019", address: "Quatre Bornes, Mauritius")
-user27 = User.create(first_name: "Neha", last_name: "Dowlut", date_of_birth: "1997-09-03", email: "neha.dowlut@example.com", password: "password123", phone: "59011098", address: "Triolet, Mauritius")
+user1 = User.create(first_name: "Aisha", last_name: "Ramdin", date_of_birth: "1993-04-12", email: "aisha.ramdin@example.com", password: "password123", phone: "57981234", address: "Quatre Bornes, Mauritius", blood_type: "A+", is_donor: true)
+user2 = User.create(first_name: "Kevin", last_name: "Appadoo", date_of_birth: "1988-11-22", email: "kevin.appadoo@example.com", password: "password123", phone: "58564321", address: "Port Louis, Mauritius", blood_type: "O-", is_donor: false)
+user3 = User.create(first_name: "Marie", last_name: "Duval", date_of_birth: "1990-02-03", email: "marie.duval@example.com", password: "password123", phone: "59210234", address: "Curepipe, Mauritius", blood_type: "B+", is_donor: true)
+user4 = User.create(first_name: "Vikash", last_name: "Rughoobur", date_of_birth: "1995-07-14", email: "vikash.rughoobur@example.com", password: "password123", phone: "57651209", address: "Vacoas, Mauritius", blood_type: "AB-", is_donor: false)
+user5 = User.create(first_name: "Sarah", last_name: "Latour", date_of_birth: "1987-09-25", email: "sarah.latour@example.com", password: "password123", phone: "59482211", address: "Rose Hill, Mauritius", blood_type: "O+", is_donor: true)
+user6 = User.create(first_name: "Jason", last_name: "Ng", date_of_birth: "1998-01-19", email: "jason.ng@example.com", password: "password123", phone: "58091344", address: "Beau Bassin, Mauritius", blood_type: "A-", is_donor: false)
+user7 = User.create(first_name: "Priya", last_name: "Seetohul", date_of_birth: "1992-06-11", email: "priya.seetohul@example.com", password: "password123", phone: "57221198", address: "Flacq, Mauritius", blood_type: "B-", is_donor: true)
+user8 = User.create(first_name: "Miguel", last_name: "Da Silva", date_of_birth: "1985-12-02", email: "miguel.dasilva@example.com", password: "password123", phone: "59734122", address: "Moka, Mauritius", blood_type: "AB+", is_donor: false)
+user9 = User.create(first_name: "Anjali", last_name: "Bhugaloo", date_of_birth: "1994-08-17", email: "anjali.bhugaloo@example.com", password: "password123", phone: "59120017", address: "Goodlands, Mauritius", blood_type: "O-", is_donor: true)
+user10 = User.create(first_name: "Dylan", last_name: "Clement", date_of_birth: "1999-03-28", email: "dylan.clement@example.com", password: "password123", phone: "57399821", address: "Mahebourg, Mauritius", blood_type: "A+", is_donor: false)
+user11 = User.create(first_name: "Stephanie", last_name: "Wong", date_of_birth: "1986-10-10", email: "stephanie.wong@example.com", password: "password123", phone: "59672210", address: "Port Louis, Mauritius", blood_type: "B+", is_donor: true)
+user12 = User.create(first_name: "Kunal", last_name: "Gopal", date_of_birth: "1991-05-05", email: "kunal.gopal@example.com", password: "password123", phone: "58412390", address: "Triolet, Mauritius", blood_type: "AB-", is_donor: false)
+user13 = User.create(first_name: "Emma", last_name: "Ramasamy", date_of_birth: "2000-02-14", email: "emma.ramasamy@example.com", password: "password123", phone: "57982011", address: "Quatre Bornes, Mauritius", blood_type: "O+", is_donor: true)
+user14 = User.create(first_name: "Yannick", last_name: "Legrand", date_of_birth: "1984-04-30", email: "yannick.legrand@example.com", password: "password123", phone: "59564401", address: "Curepipe, Mauritius", blood_type: "A-", is_donor: false)
+user15 = User.create(first_name: "Lina", last_name: "Peerbux", date_of_birth: "1997-06-08", email: "lina.peerbux@example.com", password: "password123", phone: "57812209", address: "Vacoas, Mauritius", blood_type: "B-", is_donor: true)
+user16 = User.create(first_name: "Thierry", last_name: "Labonte", date_of_birth: "1989-01-04", email: "thierry.labonte@example.com", password: "password123", phone: "59382177", address: "Grand Baie, Mauritius", blood_type: "AB+", is_donor: false)
+user17 = User.create(first_name: "Sanjana", last_name: "Mohit", date_of_birth: "1993-09-20", email: "sanjana.mohit@example.com", password: "password123", phone: "57720016", address: "Terre Rouge, Mauritius", blood_type: "O-", is_donor: true)
+user18 = User.create(first_name: "Lucas", last_name: "Robert", date_of_birth: "1990-03-10", email: "lucas.robert@example.com", password: "password123", phone: "59831190", address: "Port Louis, Mauritius", blood_type: "A+", is_donor: false)
+user19 = User.create(first_name: "Melissa", last_name: "d’Hotman", date_of_birth: "1996-07-01", email: "melissa.dhotman@example.com", password: "password123", phone: "57123098", address: "Floréal, Mauritius", blood_type: "B+", is_donor: true)
+user20 = User.create(first_name: "Harish", last_name: "Bheekhun", date_of_birth: "1988-08-09", email: "harish.bheekhun@example.com", password: "password123", phone: "57689012", address: "Phoenix, Mauritius", blood_type: "AB-", is_donor: false)
+user21 = User.create(first_name: "Amira", last_name: "Rahim", date_of_birth: "1995-11-12", email: "amira.rahim@example.com", password: "password123", phone: "59071822", address: "Quatre Bornes, Mauritius", blood_type: "O+", is_donor: true)
+user22 = User.create(first_name: "Jonathan", last_name: "Perrine", date_of_birth: "1987-02-23", email: "jonathan.perrine@example.com", password: "password123", phone: "58280117", address: "Curepipe, Mauritius", blood_type: "A-", is_donor: false)
+user23 = User.create(first_name: "Shreya", last_name: "Oodit", date_of_birth: "1998-05-09", email: "shreya.oodit@example.com", password: "password123", phone: "59320178", address: "Moka, Mauritius", blood_type: "B-", is_donor: true)
+user24 = User.create(first_name: "Andrew", last_name: "André", date_of_birth: "1985-10-16", email: "andrew.andre@example.com", password: "password123", phone: "59911032", address: "Rose Hill, Mauritius", blood_type: "AB+", is_donor: false)
+user25 = User.create(first_name: "Farah", last_name: "Abdool", date_of_birth: "1992-12-25", email: "farah.abdool@example.com", password: "password123", phone: "57192304", address: "Port Louis, Mauritius", blood_type: "O-", is_donor: true)
+user26 = User.create(first_name: "Ryan", last_name: "Cheung", date_of_birth: "1991-03-29", email: "ryan.cheung@example.com", password: "password123", phone: "58870019", address: "Quatre Bornes, Mauritius", blood_type: "A+", is_donor: false)
+user27 = User.create(first_name: "Neha", last_name: "Dowlut", date_of_birth: "1997-09-03", email: "neha.dowlut@example.com", password: "password123", phone: "59011098", address: "Triolet, Mauritius", blood_type: "B+", is_donor: true)
+
 
 puts "🌱 Seeding for blood request"
 
